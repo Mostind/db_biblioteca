91e6d3e1a75c0bfb19f60f989858984ef083c742
Mostind <joaopedroalmeida007@gmail.com>
2023-02-06 11:42:19 -0300

    versao01

---
+++ series insert.sql
@@ -0,0 +1,15 @@
+insert into series(cod_serie,num_serie,curso) 
+values ("1A","1º ano","CONTABILIDADE"),
+("1B","1º ano","DESENVOLVIMENTO DE SISTEMAS"),
+("1C","1º ano","MULTIMÍDIA"),
+("1D","1º ano","REDES DE COMPUTADORES"),
+("2A","2º ano","CONTABILIDADE"),
+("2B","2º ano","DESENVOLVIMENTO DE SISTEMAS"),
+("2C","2º ano","INFORMÁTICA"),
+("2D","2º ano","MULTIMÍDIA"),
+("2E","2º ano","REDES DE COMPUTADORES"),
+("3A","3º ano","CONTABILIDADE"),
+("3B","3º ano","DESENVOLVIMENTO DE SISTEMAS"),
+("3C","3º ano","INFORMÁTICA"),
+("3D","3º ano","MULTIMÍDIA"),
+("3E","3º ano","REDES DE COMPUTADORES");
